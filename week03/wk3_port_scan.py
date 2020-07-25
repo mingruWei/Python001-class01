2a9056923dfb0491e14917add2f162d04451737a
weimr <wmrdllg@126.com>
2020-07-25 23:22:09 +0800

    scan port

--- week03/wk3_port_scan.py
+++ week03/wk3_port_scan.py
@@ -5,22 +5,22 @@ import time
 import argparse
 import re
 import ipaddress
-from concurrent.futures import ThreadPoolExecutor
-from multiprocessing import Pool,Manager
+from multiprocessing.dummy import Pool as ThreadPool
+from multiprocessing import Pool
 
 
 result_list=[]
 json_file=''
-dict_args={}
-# print(f'dict_args id {dict_args}')
+dict_args = {}
 def write_file(result_dict):
     print(f'result_dict : {result_dict}')
-    
-    with open(json_file,'w') as f:
-        f.write(str(result_dict))
+    path = result_dict['path']
+    del result_dict['path']
+    with open(path,'a') as f:
+        f.write(str(result_dict)+"\n")
 
 def err_callback(e):
-    print('error callback: {e}')
+    print(f'error callback: {e}')
 
 
 def scan(ip_port):
@@ -33,12 +33,13 @@ def scan(ip_port):
         print(f'{ip_port} opened')
         result_dict['ip']=ip_port[0]
         result_dict['port'] = ip_port[-1]
-        # result_list.append(result_dict)
+        result_list.append(result_dict)
         # print(result_list)
     s.close()
     return result_dict
 
-def scan_p(ip_port):
+def scan_p_t(ip_port,json_file):
+  
     result_dict={}
     s = socket.socket()
     s.settimeout(0.1)
@@ -46,33 +47,37 @@ def scan_p(ip_port):
         print(f'{ip_port} opened')
         result_dict['ip']=ip_port[0]
         result_dict['port'] = ip_port[-1]
-        with open('D:/GeekUni/Python001-class01/week03/ip_port_opened.json','a') as f:
-            f.write(str(result_dict)+'\n')
-
+        result_dict['path'] = json_file
     s.close()
     return result_dict
 
+# 单进程
 def base_scan(ip_list):
-   
     for ip_i in ip_list:
-        for i in range(1, 65536):
+        for i in range(1, 200):
             scan((ip_i,i))
 # 多进程
-def base_p(ip_list,n):
-    ip_port = list(itertools.product(ip_list,range(1, 200)))
+def base_p(ip_list,n,js_filepath):
+    # ip_port = list(itertools.product(ip_list,range(1, 200)))
     with Pool(processes=n) as pool:
-        pool.map(scan_p,ip_port) 
-        # for ip in ip_list:
-        #     for i in range(1,200):
-        #         pool.apply_async(scan,args=((ip,i)),callback=write_file,error_callback=err_callback)
+    #     pool.map(scan_p,ip_port) 
+        for ip in ip_list:
+            for i in range(1,65536):
+                time.sleep(0.1)
+                pool.apply_async(scan_p_t,args=((ip,i),js_filepath),callback=write_file,error_callback=err_callback)
 
 
 # 多线程   
-def base_t(ip_list,n):
-    ip_port = list(itertools.product(ip_list,range(1, 200)))
-    with ThreadPoolExecutor(n) as executor:
-        executor.map(scan_p,ip_port)
-
+def base_t(ip_list,n,js_filepath):
+    # ip_port = list(itertools.product(ip_list,range(1, 200)))
+    # with ThreadPool(n) as pool:
+    #     pool.map(scan_p, ip_port)
+    with ThreadPool(processes=n) as pool:
+        for ip in ip_list:
+            for i in range(1,65536):
+                time.sleep(0.1)
+                pool.apply_async(scan_p_t,args=((ip,i),js_filepath),callback=write_file,error_callback=err_callback)
+# 命令行参数解析
 def parse():
     parser = argparse.ArgumentParser()
     parser.add_argument("-n", help="set the number of Concurrent ",required=True)
@@ -85,6 +90,7 @@ def parse():
     args = parser.parse_args()
     return args
 
+# 解析参数存入字典
 def get_param(args):
     global dict_args
 
@@ -97,9 +103,11 @@ def get_param(args):
     if n is not None:
         n = int(args.n)
     dict_args['n'] = n
-    print(f'-------json_file{id(dict_args)}')
     return dict_args
+
+# 处理连续地址段
 def ip_address_split(ip_str):
+    
     ip_list=[]
     if ip_str is not None:
         pattern = re.compile(r'(.*)\-(.*?)$') 
@@ -111,31 +119,28 @@ def ip_address_split(ip_str):
             ip_start = ip_start+1
     return ip_list
 if __name__ == "__main__":
-
     args = parse()
     print(args.w)
     dict_args =  get_param(args)
-    print(f'++++++++++++++++++++++json_file{dict_args}')
     ip_list= ip_address_split(dict_args['ip'])
 
+# 执行方式判断 
     if  dict_args['m'] =='proc':
         if args.v is not None:
-            t1 = timeit(stmt = base_p(ip_list,dict_args['n']),setup = 'from __main__ import base_p', number = 1)
+            t1 = timeit(stmt = base_p(ip_list,dict_args['n'],dict_args['w']),setup = 'from __main__ import base_p', number = 1)
             print(f'多进程耗时：{t1} 秒')
         else:
             print('多进程扫描')
-            base_p(ip_list,dict_args['n'])
-        # with open(json_file,'w') as f:
-        #     f.write(str(result_list))
+            base_p(ip_list,dict_args['n'],dict_args['w'])
+
     elif  dict_args['m'] =='thread':
         if args.v is not None:
-            t2 = timeit(stmt = base_t(ip_list,dict_args['n']), setup = 'from __main__ import base_t', number = 1)
+            t2 = timeit(stmt = base_t(ip_list,dict_args['n'],dict_args['w']), setup = 'from __main__ import base_t', number = 1)
             print(f'多线程耗时：{t2} 秒')
         else:
             print('多线程扫描')
-            base_t(ip_list,dict_args['n'])
-        # with open(json_file,'w') as f:
-        #     f.write(str(result_list))
+            base_t(ip_list,dict_args['n'],dict_args['w'])
+
     else:
         if args.v is not None: 
             t3 = timeit(stmt = base_scan(ip_list),setup = 'from __main__ import base_scan', number = 1)
@@ -143,6 +148,6 @@ if __name__ == "__main__":
         else:
             print('单进程扫描') 
             base_scan(ip_list)
-        with open(json_file,'w') as f:
+        with open(dict_args['w'],'w') as f:
             f.write(str(result_list))
  
